c3389be5ad84c38e6a982b6cf40637c21afa3fe4
Ntshangase <simphiwe323i@gmail.com>
2024-08-24 15:32:06 +0200

    a win is a win

--- src/Components/RepairNavbar.js
+++ src/Components/RepairNavbar.js
@@ -1,14 +1,21 @@
-
-import React from 'react'
+import React from 'react';
+import './RepairNavbar.css';
+import { Link } from 'react-router-dom';
 
 export default function RepairNavbar() {
   return (
-    <div>
-        <nav>
-			<a href="/">Home</a>
-			<a href="/RepairLanding">Landing</a>
-			<a href="/UpdateQuote">UpdateQuote</a>
-		</nav>
+    <nav className="navbar">
+    <div className="navbar-brand">
+      <Link to="/">Track My Repair</Link>
     </div>
-  )
+    <ul className="navbar-nav">
+      <li className="nav-item">
+        <Link to="/RepairLanding">Landing Page</Link>
+      </li>
+      <li className="nav-item">
+        <Link to="/UpdateQuote">Timeline</Link>
+      </li>
+    </ul>
+  </nav>
+  );
 }
