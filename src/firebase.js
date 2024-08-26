61820166626f1d99fa7d8b1802d82afb985f6741
Ntshangase <simphiwe323i@gmail.com>
2024-08-26 16:19:07 +0200

    invisible

--- src/firebase.js
+++ src/firebase.js
@@ -10,15 +10,15 @@ import { getDatabase } from 'firebase/database';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAwFE6VHrYPcgkX7ruXPIonSLrA6IU9fDM",
-  authDomain: "winner-4da95.firebaseapp.com",
-  projectId: "winner-4da95",
-  databaseURL: "https://winner-4da95-default-rtdb.firebaseio.com/",
-  storageBucket: "winner-4da95.appspot.com",
-  messagingSenderId: "537322194131",
-  appId: "1:537322194131:web:8a3087b8666714a65f8026",
-  measurementId: "G-D24FR52F2F"
-};
+    apiKey: "AIzaSyAwFE6VHrYPcgkX7ruXPIonSLrA6IU9fDM",
+    authDomain: "winner-4da95.firebaseapp.com",
+    databaseURL: "https://winner-4da95-default-rtdb.firebaseio.com",
+    projectId: "winner-4da95",
+    storageBucket: "winner-4da95.appspot.com",
+    messagingSenderId: "537322194131",
+    appId: "1:537322194131:web:8a3087b8666714a65f8026",
+    measurementId: "G-D24FR52F2F"
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
