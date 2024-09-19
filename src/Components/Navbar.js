73f990e0ca7e768f2aa431abe2c2663d4bba13ac
Ntshangase <simphiwe323i@gmail.com>
2024-09-19 15:41:38 +0200

    css ayikapheli

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css'
+import './navbar.css';
 
 export default function Navbar() {
+  const [isNavActive, setIsNavActive] = useState(false);
+
+  const handleToggle = () => {
+    setIsNavActive(!isNavActive);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">Track My Repair</Link>
+        <span className="navbar-toggle" onClick={handleToggle}>
+          &#9776;
+        </span>
       </div>
-      <ul className="navbar-nav">
+      <ul className={`navbar-nav ${isNavActive ? 'active' : ''}`}>
         <li className="nav-item">
           <Link to="/CustomerLanding">Landing Page</Link>
         </li>
