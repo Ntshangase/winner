f109182fb9c46a7f589cca4182908bb49c65ac29
Ntshangase <simphiwe323i@gmail.com>
2024-09-19 16:47:02 +0200

    perfect repairNavbar

--- src/Components/RepairNavbar.js
+++ src/Components/RepairNavbar.js
@@ -1,21 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RepairNavbar.css';
 import { Link } from 'react-router-dom';
 
 export default function RepairNavbar() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  const toggleNav = () => {
+    setIsNavOpen(!isNavOpen);
+  };
+
   return (
     <nav className="navbar">
-    <div className="navbar-brand">
-      <Link to="/">Track My Repair</Link>
-    </div>
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link to="/RepairLanding">Landing Page</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/UpdateQuote">Update Order</Link>
-      </li>
-    </ul>
-  </nav>
+      <div className="navbar-brand">
+        <Link to="/">Track My Repair</Link>
+        <button className="navbar-toggle" onClick={toggleNav}>
+        ☰
+      </button>
+      </div>
+      <ul className={`navbar-nav ${isNavOpen ? 'open' : ''}`}>
+        <li className="nav-item">
+          <Link to="/RepairLanding">Landing Page</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/UpdateQuote">Update Order</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
