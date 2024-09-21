eedffe6f8cee9a354a1a34c0131585e7720773e3
Ntshangase <simphiwe323i@gmail.com>
2024-09-21 23:51:56 +0200

    on fire, repair navbar responsive

--- src/Components/RepairNavbar.js
+++ src/Components/RepairNavbar.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './RepairNavbar.css';
 import { Link } from 'react-router-dom';
 
 export default function RepairNavbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
-    <div className="navbar-brand">
-      <Link to="/">Track My Repair</Link>
-    </div>
-    <ul className="navbar-nav">
-      <li className="nav-item">
-        <Link to="/RepairLanding">Landing Page</Link>
-      </li>
-      <li className="nav-item">
-        <Link to="/UpdateQuote">Update Order</Link>
-      </li>
-    </ul>
-  </nav>
+      <div className="navbar-brand">
+        <Link to="/">Track My Repair</Link>
+      </div>
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+      </div>
+      <ul className={`navbar-nav ${isOpen ? 'open' : ''}`}>
+        <li className="nav-item">
+          <Link to="/RepairLanding">Landing Page</Link>
+        </li>
+        <li className="nav-item">
+          <Link to="/UpdateQuote">Update Order</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
-
-/*test*/
