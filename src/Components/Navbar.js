022814cd47feb02676b4bdef8179516f47215e25
Ntshangase <simphiwe323i@gmail.com>
2024-09-21 23:27:51 +0200

    navbar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './navbar.css'
+import './navbar.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-brand">
         <Link to="/">Track My Repair</Link>
       </div>
-      <ul className="navbar-nav">
+      <div className="navbar-toggle" onClick={toggleMenu}>
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+        <span className="hamburger"></span>
+      </div>
+      <ul className={`navbar-nav ${isOpen ? 'open' : ''}`}>
         <li className="nav-item">
           <Link to="/CustomerLanding">Landing Page</Link>
         </li>
@@ -19,5 +30,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-// test
